4b693b040899279a96381403d7341fdf2142f9a3
Ziqi <ziqi.francis.fan@gmail.com>
2019-12-12 14:37:25 -0500

    added command line tools for inverse problem

--- main.cpp
+++ main.cpp
@@ -23,6 +23,7 @@ extern float intwgt[INTORDER];
 
 int main(int argc, char *argv[])
 {
+    /*
     HOST_CALL(cuGenGaussParams(INTORDER,intpt,intwgt));
     HOST_CALL(gaussPtsToDevice(intpt,intwgt));
     SetHostBases();
@@ -50,7 +51,128 @@ int main(int argc, char *argv[])
             //mag,src_loc,4,rect,0.01,"./data/vox","./data/loudness"));
     HOST_CALL(WriteZSliceVoxLoudness("./mesh/test1.obj",band,"point",mag,src_loc,4,
             zCoord,len,rect_2d,"./data/vox","./data/loudness"));
+    */
     
+    int c;
+    int option_index = 0;
+    char obj_file[50] = "./mesh/test.obj", src_type[50] = "point", vox_file[50] = "./data/vox", 
+            field_file[50] = "./data/lu";
+    double z_coord, len, radius = 3.0, src_mag = 1.0, x_cnr = -5, y_cnr = -5, 
+            x_len = 10, y_len = 10;
+    float band[2];
+    int src_num = 4;
+    aarect2d rect = {x_cnr,y_cnr,x_len,y_len};
+    struct option long_options[] = {
+        {"obj_file", required_argument, NULL, 0},
+        {"src_type", required_argument, NULL, 1},
+        {"vox_file", required_argument, NULL, 2},
+        {"field_file", required_argument, NULL, 3},
+        {"low_ng_freq", required_argument, NULL, 4},
+        {"up_ng_freq", required_argument, NULL, 5},
+        {"src_radi", required_argument, NULL, 6},
+        {"src_num", required_argument, NULL, 7},
+        {"src_mag", required_argument, NULL, 8},
+        {"x_cnr", required_argument, NULL, 9},
+        {"y_cnr", required_argument, NULL, 10},
+        {"x_len", required_argument, NULL, 11}, 
+        {"y_len", required_argument, NULL, 12},
+        {"z_coord", required_argument, NULL, 13},
+        {"side_len", required_argument, NULL, 14},
+        {0,0,0,0}
+    };
+    // parse command line arguments
+    while(1) {
+        c = getopt_long(argc,argv,"",long_options,&option_index);
+        if(c == -1) 
+            break;
+        switch(c) {
+            case 0:
+                strcpy(obj_file,optarg);
+                printf("file name: %s\n",obj_file);
+                break;
+            case 1:
+                strcpy(src_type,optarg);
+                printf("source type: %s\n",src_type);
+                break;
+            case 2:
+                strcpy(vox_file,optarg);
+                printf("vox file: %s\n",vox_file);
+                break;
+            case 3:
+                strcpy(field_file,optarg);
+                printf("field file: %s\n",field_file);
+                break;
+            case 4:
+                band[0] = atof(optarg);
+                printf("lower frequency bound: %lf\n",band[0]);
+                break;
+            case 5:
+                band[1] = atof(optarg);
+                printf("frequency interpolation: %lf\n",band[1]);
+                break;
+            case 6:
+                radius = atof(optarg);
+                printf("radius of sources: %lf\n",radius);
+                break;
+            case 7:
+                src_num = atof(optarg);
+                printf("number of sources: %d\n",src_num);
+                break;
+            case 8:
+                src_mag = atof(optarg);
+                printf("magnitude of sources: %lf\n",src_mag);
+                break;
+            case 9:
+                x_cnr = atof(optarg);
+                printf("x coordinate of the corner: %lf\n",x_cnr);
+                break;
+            case 10:
+                y_cnr = atof(optarg);
+                printf("y coordinate of the corner: %lf\n",y_cnr);
+                break;
+            case 11:
+                x_len = atof(optarg);
+                printf("length in x direction: %lf\n",x_len);
+                break;
+            case 12:
+                y_len = atof(optarg);
+                printf("length in y direction: %lf\n",y_len);
+                break;
+            case 13:
+                z_coord = atof(optarg);
+                printf("coordinate of z slice: %lf\n",z_coord);
+                break;
+            case 14:
+                len = atof(optarg);
+                printf("side length: %lf\n",len);
+                break;
+            default:
+                printf("The current option is not recognized.\n");
+        }   
+    }
+    
+    // set up sources
+    vec3f *src = (vec3f*)malloc(src_num*sizeof(vec3f));
+    for(int i=0;i<src_num;i++) {
+        double theta = 2*PI/src_num*i;
+        double x = radius*cos(theta);
+        double y = radius*sin(theta);
+        src[i].coords[0] = x;
+        src[i].coords[1] = y;
+        src[i].coords[2] = z_coord;
+    }
+    float *mag = (float*)malloc(src_num*sizeof(float));
+    for(int i=0;i<src_num;i++) {
+        mag[i] = src_mag;
+    }
+    rect.cnr.coords[0] = x_cnr;
+    rect.cnr.coords[1] = y_cnr;
+    rect.len[0] = x_len;
+    rect.len[1] = y_len;
+    HOST_CALL(WriteZSliceVoxLoudness(obj_file,band,"point",mag,src,src_num,
+            z_coord,len,rect,"./data/vox","./data/loudness"));
     CUDA_CALL(cudaDeviceReset());
+    free(mag);
+    free(src);
     return EXIT_SUCCESS;
 }
