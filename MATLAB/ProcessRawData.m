2b3ba5efde5d7c3e8a2a5191e86d68353f5b733b
Ziqi <ziqi.francis.fan@gmail.com>
2019-12-14 01:08:38 -0500

    created matlab script to visualize all loudness fields

---
+++ MATLAB/ProcessRawData.m
@@ -0,0 +1,26 @@
+function [grid,field] = ProcessRawData(grid_path,field_path,dark_len,dark_label)
+%ProcessRawData Summary of this function goes here
+%   Reads the raw data of an occupancy grid and a field to get matlab
+%   matrices
+fileID = fopen(grid_path,'r');
+dim_sz = fread(fileID,3,'int');
+grid = fread(fileID,dim_sz(1)*dim_sz(2)*dim_sz(3),'int');
+fclose(fileID);
+
+grid = reshape(grid,[dim_sz(1),dim_sz(2),dim_sz(3)]);
+
+fileID = fopen(field_path,'r');
+dim_sz = fread(fileID,3,'int');
+field = fread(fileID,dim_sz(1)*dim_sz(2)*dim_sz(3),'single');
+fclose(fileID);
+
+field = reshape(field,[dim_sz(1),dim_sz(2),dim_sz(3)]);
+
+disp(min(min(field)));
+
+len = (dim_sz(1)-dark_len)/2;
+
+field(len+1:len+dark_len,len+1:len+dark_len) = dark_label;
+
+end
+
---
+++ MATLAB/visualize_data.m
@@ -0,0 +1,31 @@
+%% This script visualizes data generated from the data generator
+clear; close all; clc;
+
+%% specify the path
+folder = '../data/';
+grid_name = 'vox';
+field_name_base = 'loudness';
+
+grid_path = [folder,grid_name];
+numLoudness = 8;
+fig1 = figure;
+ax1 = axes(fig1);
+daspect(ax1,[1,1,1]);
+set(ax1,'visible','off');
+fig2 = figure;
+ax2 = axes(fig2);
+for i=1:numLoudness
+    field_name = [field_name_base,'_',num2str(i-1)];
+    field_path = [folder,field_name];
+    [grid,field] = ProcessRawData(grid_path,field_path,100,-5);
+    caxis(ax1,[-10,10]);
+    imagesc(ax1,field);
+    set(ax1,'visible','off');
+    daspect(ax1,[1,1,1]);
+    pause(1.0);
+    if i==1
+        imagesc(ax2,grid);
+        set(ax2,'visible','off');
+        daspect(ax2,[1,1,1]);
+    end
+end
