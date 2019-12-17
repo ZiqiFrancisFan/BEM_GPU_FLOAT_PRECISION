47ce971eb67d669d0f4f7253e7151b2cb0c945ff
Ziqi <ziqi.francis.fan@gmail.com>
2019-12-17 01:08:12 -0500

    fixed buffer overflow issue

--- GenData.sh
+++ GenData.sh
@@ -49,8 +49,8 @@ do
             for (( idx=0; idx<numEachType; idx++ ))
             do
                 curr_idx=$(((num_side-3)*numEachType+idx))
-                ./pipline_poly --edge_no="$num_side" --radius="${radius}" --hight="${height}" --freq_max="${freq_max}"\
-                    --output_file_path="${base}/${group}/mesh/poly_${curr_idx}.obj"
+                ./pipeline_poly --edge_no="$num_side" --radius="${radius}" --height="${height}" --freq_max="${freq_max}"\
+                    --output_path="${base}/${group}/mesh/poly${curr_idx}.obj"
             done
         done
     fi
@@ -64,8 +64,8 @@ do
                 #echo "idx: ${idx}"
                 curr_idx=$(((num_side-3)*numEachType+idx))
                 #echo "current index: $curr_idx"
-                ./pipline_poly --edge_no="$num_side" --radius="${radius}" --hight="${height}" --freq_max="${freq_max}"\
-                    --output_file_path="${base}/${group}/mesh/poly_${curr_idx}.obj"
+                ./pipeline_poly --edge_no="$num_side" --radius="${radius}" --height="${height}" --freq_max="${freq_max}"\
+                    --output_path="${base}/${group}/mesh/poly${curr_idx}.obj"
             done
         done
     fi
@@ -78,15 +78,15 @@ do
             do
                 #echo "idx: ${idx}"
                 curr_idx=$(((num_side-3)*numEachType+idx))
-                ./pipline_poly --edge_no="$num_side" --radius="${radius}" --hight="${height}" --freq_max="${freq_max}"\
-                    --output_file_path="${base}/${group}/mesh/poly_${curr_idx}.obj"
+                ./pipeline_poly --edge_no="$num_side" --radius="${radius}" --height="${height}" --freq_max="${freq_max}"\
+                    --output_path="${base}/${group}/mesh/poly${curr_idx}.obj"
             done
         done
     fi
 done
 
 # generate loudness fields
-
+#read -p "Press [Enter] key to start backup..."
 for group in "train" "validation" "test"
 do
     if [ ${group} == "train" ]; then
@@ -96,42 +96,52 @@ do
         do
             for (( idx=0; idx<numEachType; idx++ ))
             do
+                echo "mesh path: ${base}/${group}/mesh/poly${curr_idx}.obj"
+                echo "vox path: ${base}/${group}/input/raw/vox${curr_idx}"
+                echo "field path: ${base}/${group}/output/raw/field${curr_idx}"
                 curr_idx=$(((num_side-3)*numEachType+idx))
-                ./main --obj_file="${base}/${group}/mesh/poly_${curr_idx}.obj" --src_type="point" --src_radi=5 --src_num=8 --oct_num=4\
+                ./main --obj_file="${base}/${group}/mesh/poly${curr_idx}.obj" --src_type="point" --src_radi=5 --src_num=8 --oct_num=4\
                 --src_mag=1.0 --x_cnr=-2.56 --y_cnr=-2.56 --x_len=5.12 --y_len=5.12 --z_coord=0.5 --side_len=0.01\
-                --vox_file="${base}/${group}/input/raw/vox${curr_idx}" --field_file= "${base}/${group}/output/raw/field${curr_idx}"
+                --vox_file="${base}/${group}/input/raw/vox${curr_idx}" --field_file="${base}/${group}/output/raw/field${curr_idx}"
+                #read -p "Press [Enter] key to start backup..."
             done
         done
     fi
     if [ ${group} == "validation" ]; then
         #echo "generating mesh for val"
         numEachType=numPolyEachType_val
-        for (( num_side=3; num_side<=10; num_side++ ))
+        for (( num_side=lowNumSide; num_side<=upNumSide; num_side++ ))
         do
             for (( idx=0; idx<numEachType; idx++ ))
             do
+                echo "mesh path: ${base}/${group}/mesh/poly${curr_idx}.obj"
+                echo "vox path: ${base}/${group}/input/raw/vox${curr_idx}"
+                echo "field path: ${base}/${group}/output/raw/field${curr_idx}"
                 #echo "idx: ${idx}"
                 curr_idx=$(((num_side-3)*numEachType+idx))
-                curr_idx=$(((num_side-3)*numEachType+idx))
-                ./main --obj_file="${base}/${group}/mesh/poly_${curr_idx}.obj" --src_type="point" --src_radi=5 --src_num=8 --oct_num=4\
+                ./main --obj_file="${base}/${group}/mesh/poly${curr_idx}.obj" --src_type="point" --src_radi=5 --src_num=8 --oct_num=4\
                 --src_mag=1.0 --x_cnr=-2.56 --y_cnr=-2.56 --x_len=5.12 --y_len=5.12 --z_coord=0.5 --side_len=0.01\
-                --vox_file="${base}/${group}/input/raw/vox${curr_idx}" --field_file= "${base}/${group}/output/raw/field${curr_idx}"
+                --vox_file="${base}/${group}/input/raw/vox${curr_idx}" --field_file="${base}/${group}/output/raw/field${curr_idx}"
+                #read -p "Press [Enter] key to start backup..."
             done
         done
     fi
     if [ ${group} == "test" ]; then
         #echo "generating mesh for test"
         numEachType=numPolyEachType_test
-        for (( num_side=3; num_side<=10; num_side++ ))
+        for (( num_side=lowNumSide; num_side<=upNumSide; num_side++ ))
         do
             for (( idx=0; idx<numEachType; idx++ ))
             do
+                echo "mesh path: ${base}/${group}/mesh/poly${curr_idx}.obj"
+                echo "vox path: ${base}/${group}/input/raw/vox${curr_idx}"
+                echo "field path: ${base}/${group}/output/raw/field${curr_idx}"
                 #echo "idx: ${idx}"
                 curr_idx=$(((num_side-3)*numEachType+idx))
-                curr_idx=$(((num_side-3)*numEachType+idx))
-                ./main --obj_file="${base}/${group}/mesh/poly_${curr_idx}.obj" --src_type="point" --src_radi=5 --src_num=8 --oct_num=4\
+                ./main --obj_file="${base}/${group}/mesh/poly${curr_idx}.obj" --src_type="point" --src_radi=5 --src_num=8 --oct_num=4\
                 --src_mag=1.0 --x_cnr=-2.56 --y_cnr=-2.56 --x_len=5.12 --y_len=5.12 --z_coord=0.5 --side_len=0.01\
-                --vox_file="${base}/${group}/input/raw/vox${curr_idx}" --field_file= "${base}/${group}/output/raw/field${curr_idx}"
+                --vox_file="${base}/${group}/input/raw/vox${curr_idx}" --field_file="${base}/${group}/output/raw/field${curr_idx}"
+                #read -p "Press [Enter] key to start backup..."
             done
         done
     fi
