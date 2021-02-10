a7c7f7c80c92025c9b40a4f1986ed49a317036df
ziqi <ziqi.francis.fan@gmail.com>
2021-02-09 20:03:54 -0500

    added copyright statements

--- octree.h
+++ octree.h
@@ -1,14 +1,7 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/* 
- * File:   octree.h
- * Author: ziqi
- *
- * Created on March 5, 2019, 9:15 AM
+ ©Copyright 2020 University of Florida Research Foundation, Inc. All Commercial Rights Reserved.
+ For commercial license interest please contact UF Innovate | Tech Licensing, 747 SW 2nd Avenue, P.O. Box 115575, Gainesville, FL 32601,
+ Phone (352) 392-8929 and reference UF technology T18423.
  */
 
 #ifndef OCTREE_H
